eb63e9e5eb31637bb1e796311ddc672e4ad8e3f4
Gall Steinitz <gall.steinitz@datagrail.io>
2024-04-16 22:54:28 -0700

    add copy button and automatically select javascript

--- docs/assets/index.js
+++ docs/assets/index.js
@@ -1,8 +1,12 @@
 !function () {
   this.main = function () {
+    const that = this;
     makeHTTPRequest('assets/aita_reader_bookmarklet_0.1.js', function(event) {
       processRequest('#aita_reader_bookmarklet', event)
     });
+    window.onload = function() {
+      that.setupCopyButton();
+    }
   }
 
   this.makeHTTPRequest = function (url, callback) {
@@ -25,6 +29,23 @@
   this.checkResponse = function(domId, response) {
     document.querySelector(domId).href=response;
     document.querySelector('#js-textarea').textContent = response;
+  }
+
+  this.setupCopyButton = function() {
+    // Select the textarea and the button
+    const textarea = document.getElementById('js-textarea');
+    const copyButton = document.getElementById('copy-button');
+
+    // Add a click event listener to the textarea to select its content
+    textarea.addEventListener('click', function() {
+      this.select();
+    });
+
+    // Add a click event listener to the button to copy the textarea content
+    copyButton.addEventListener('click', function() {
+      textarea.select();
+      document.execCommand('copy');
+    });
   }
 
   main();
